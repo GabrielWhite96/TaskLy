e6e442867e0f1874866140a36be6c7cd4309ff4d
Gabriel Fernandes <gabrielfvp32015@gmail.com>
2024-10-05 11:29:08 -0300

    change the sequence of container

--- src/components/EisenhowerMatrixSectionLP/index.jsx
+++ src/components/EisenhowerMatrixSectionLP/index.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React from "react";
 import {
   EisenhowerSectionContainer,
   EisenhowerTextContainer,
   EisenhowerTitle,
   EisenhowerSubtitle,
   EisenhowerImage,
-} from './EisenhowerSectionLP.styles';
-import eisenhowerImage from '../../assets/task_example.png'; // Substitua pela imagem do app
+} from "./EisenhowerSectionLP.styles";
+import eisenhowerImage from "../../assets/task_example.png"; // Substitua pela imagem do app
 
 function EisenhowerSectionLP() {
   return (
     <EisenhowerSectionContainer>
+      <EisenhowerImage
+        src={eisenhowerImage}
+        alt="App showing Eisenhower matrix feature"
+      />
       <EisenhowerTextContainer>
-        <EisenhowerTitle>Controle suas prioridades com a Matriz de Eisenhower</EisenhowerTitle>
+        <EisenhowerTitle>
+          Controle suas prioridades com a Matriz de Eisenhower
+        </EisenhowerTitle>
         <EisenhowerSubtitle>
-          Organize suas tarefas com base na urgência e importância. O app permite que você defina as prioridades de cada tarefa, ajudando a focar no que realmente importa.
+          Organize suas tarefas com base na urgência e importância. O app
+          permite que você defina as prioridades de cada tarefa, ajudando a
+          focar no que realmente importa.
         </EisenhowerSubtitle>
       </EisenhowerTextContainer>
-      <EisenhowerImage src={eisenhowerImage} alt="App showing Eisenhower matrix feature" />
     </EisenhowerSectionContainer>
   );
 }
