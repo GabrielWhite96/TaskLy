6ab6d99f8c71c9b10a86e5ea7513365a43e66f35
Gabriel Fernandes <gabrielfvp32015@gmail.com>
2024-10-08 14:33:12 -0300

    add new colors theme

--- src/styles/themes/blueDark.js
+++ src/styles/themes/blueDark.js
@@ -5,8 +5,11 @@ export default {
     primary: "#006ba6",
     secondary: "#0496ff",
     textHover: "#f5f5f5",
+    backgroundInput: "#333",
     background: "#222",
     backgroundSecondary: "#1d1d1d",
+    backgroundTertiary: "#141414",
     text: "#f5f5f5",
+    subText: "#666",
   },
 };
--- src/styles/themes/greenDark.js
+++ src/styles/themes/greenDark.js
@@ -5,8 +5,11 @@ export default {
     primary: "#7eb509",
     secondary: "#9cbe54",
     textHover: "#f5f5f5",
+    backgroundInput: "#333",
     background: "#222",
     backgroundSecondary: "#1d1d1d",
+    backgroundTertiary: "#141414",
     text: "#f5f5f5",
+    subText: "#666",
   },
 };
